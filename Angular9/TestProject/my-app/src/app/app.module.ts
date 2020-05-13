ddf480c1c06167f57d449b4ca6c4e0b7678f10c6
david <davidjohnnetwork@gmail.com>
2020-05-13 22:39:27 +0530

    Day 7: Angular project

--- Angular9/TestProject/my-app/src/app/app.module.ts
+++ Angular9/TestProject/my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
