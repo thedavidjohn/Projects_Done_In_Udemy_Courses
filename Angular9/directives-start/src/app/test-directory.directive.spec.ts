e1fe92406deb5be07b90dab69b830b4edc04016d
david <davidjohnnetwork@gmail.com>
2020-05-14 06:09:08 +0530

    Day 7 : Services and Directives in Angular

---
+++ Angular9/directives-start/src/app/test-directory.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TestDirectoryDirective } from './test-directory.directive';
+
+describe('TestDirectoryDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TestDirectoryDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ Angular9/directives-start/src/app/test-directory.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appTestDirectory]'
+})
+export class TestDirectoryDirective {
+
+  constructor() { }
+
+}
